1b649fb97048ed258fc353f3383afbd66a91e49d
Akhilesh <akhilesh2113@gmail.com>
2025-06-20 10:32:13 +0530

    Added mongo uri

--- app.js
+++ app.js
@@ -7,27 +7,22 @@ import cors from "cors";
 
 const app = express();
 app.use(bodyParser.json());
-app.use(cors())
+app.use(cors());
 dotenv.config();
 
 const PORT = process.env.PORT || 3000;
-const MONGOURL = process.env.MONGO_URL;
-
-const DB_OPTION = {
-    user:'root',
-    pass:'yourStrongPassword',
-    dbName:'Rent_Agreement',
-    authSource:'admin'
-}
+const MONGOURL = process.env.MONGODB_URI;
 
 mongoose
-.connect(MONGOURL,DB_OPTION)
-.then(
-    ()=>{console.log("DB connected successfully")}
-)
-.catch(err => console.log(err))
+  .connect(MONGOURL, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  })
+  .then(() => console.log("MongoDB Atlas connected successfully"))
+  .catch((err) => console.error("Connection error:", err));
 
-app.use("/api",route);
+app.use("/api", route);
 
-app.listen(PORT,()=>{console.log(`Listening at port http://localhost:${PORT}`)});
-
+app.listen(PORT, () => {
+  console.log(`Server running at http://localhost:${PORT}`);
+});
